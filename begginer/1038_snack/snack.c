ffb7963cbf07b5dfc8c53772d8d7e06d30b5ecfb
KogawaV <=>
2019-10-24 22:44:00 -0300

    upload_2 uri_judge begginer - modificacao exercico 1038(snack)

--- begginer/1038_snack/snack.c
+++ begginer/1038_snack/snack.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 //protótipos das funções
-
+double calc_price(int opcao, int quantidade);
 
 int main(){
 	int opcao = 0, quantidade = 0, preco_final = 0;
@@ -13,49 +13,48 @@ int main(){
 	printf("5   Refrigerante    R$1.50\n");
 	
 	scanf("%d %d", &opcao, &quantidade);
-	printf("Opção: %d\nQuantidade: %d\n", opcao, quantidade);
+	//printf("Opção: %d\nQuantidade: %d\n", opcao, quantidade);
 
-	/*switch(opcao){
+	calc_price(opcao, quantidade);
+
+	return 0;
+}
+
+double calc_price(int opcao, int quantidade){
+	double preco_final = 0.00;
+
+	switch(opcao){
 		case 1:
-			scanf("%d", &quantidade);
 			preco_final = 4 * quantidade;
 
-			printf("Total: R$ %d\n", preco_final);			
+			printf("Total: R$ %.2lf\n", preco_final);			
 			break;
 
 		case 2:
-			scanf("%d", &quantidade);
 			preco_final = 4.5 * quantidade;
 
-			printf("Total: R$ %d\n", preco_final);			
+			printf("Total: R$ %.2lf\n", preco_final);			
 			break;
 		case 3:
-			scanf("%d", &quantidade);
 			preco_final = 5 * quantidade;
 
-			printf("Total: R$ %d\n", preco_final);			
+			printf("Total: R$ %.2lf\n", preco_final);			
 			break;
 
 		case 4:
-			scanf("%d", &quantidade);
 			preco_final = 2 * quantidade;
 
-			printf("Total: R$ %d\n", preco_final);			
+			printf("Total: R$ %.2lf\n", preco_final);			
 			break;
 
 		case 5:
-			scanf("%d", &quantidade);
 			preco_final = 1.5 * quantidade;
 
-			printf("Total: R$ %d\n", preco_final);			
+			printf("Total: R$ %.2lf\n", preco_final);			
 			break;
 		default:
 			printf("opção inválida");
 			break;
-	}*/
-}
-
-double calc_price(){
-
+	}
 }
 
