c70d6f7db642fab32d6543a51b2b0ab4e75ef42e
KogawaV <=>
2019-10-29 08:16:33 -0300

    uri_judge begginer triangulo_1043

---
+++ begginer/1043_triangulo/triangulo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+double perimetro_triangulo(double lado_a, double lado_b, double lado_c);
+double area_trapezio(double base_a, double base_b, double altura_c);
+
+int main(){
+    double lado_a = 0.00, lado_b = 0.00, lado_c = 0.00;
+
+    scanf("%lf %lf %lf", &lado_a, &lado_b, &lado_c);
+
+    //testando função que retorna o valor absoluto de um determinada variável
+    /*double abs_lado_a = fabs(lado_a);
+    printf("Absoluto A: %lf.\n", abs_lado_a);*/
+
+    if((fabs(lado_b - lado_c) < lado_a) && ((lado_b + lado_c) > lado_a) && ((fabs(lado_a - lado_c) < lado_b) && ((lado_a + lado_c) > lado_b)) && ((fabs(lado_a - lado_b) < lado_c) && ((lado_a + lado_b) > lado_c))){
+        printf("é um triângulo.");
+    }else{
+        printf("é um trapézio.");
+    }
+
+    return 0;
+}
+
+double perimetro_triangulo(double lado_a, double lado_b, double lado_c){}
+
+double area_trapezio(double base_a, double base_b, double altura_c){}
